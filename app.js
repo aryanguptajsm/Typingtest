d598ca4bdced7d90974324abb9bb2fce04cf67de
Aryan gupta <aryangupta.jsm@gmail.com>
2026-07-21 22:46:09 +0530

    refactor: restructure runtime function and event listener for improved clarity

--- app.js
+++ app.js
@@ -2,18 +2,24 @@
     const inputField = document.getElementById('input');
     const resultText = document.getElementById('result');
     const text = document.getElementById("text");
-    
-    startBtn.addEventListener('click', () => {
-      const value = inputField.value.trim();
-      let time = 0;
-     let run = setInterval(()=>{
+    let run;
+     function runtime(){
+       let time = 0;
+     run = setInterval(()=>{
          time++;
          resultText.textContent = (time / 100).toFixed(2);
      },10) 
+    };
+    startBtn.addEventListener('click', () => {
+      let value = inputField.value.trim();
+       console.dir(value);
+       runtime();
+        clearInterval(run);
+       startBtn.textContent = "stop";
       if (value === text) {
-          clearInterval(run);
-      } else {
-        resultText.textContent = 'Please enter a value first.';
-      }
+         
+      } 
     });
-    console.log("value");
+   
+    
+   
