2805fb99145f6bf01baa4d4d71580b3716678225
Aryan gupta <aryangupta.jsm@gmail.com>
2026-07-26 23:40:18 +0530

    fix: correct spelling of accuracy variable in app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const con = document.querySelector(".container");
+const inf = con.querySelector(".info");
+
+const startBtn = con.querySelector("#start");
+const inputField = con.querySelector("#input");
+const resultText = con.querySelector("#result");
+const text = con.querySelector("#text");
+
+const times = inf.querySelector("#time");
+const character = inf.querySelector("#characters");
+const restart = con.querySelector("#restart");
+const accurace = inf.querySelector("#accuracy");
